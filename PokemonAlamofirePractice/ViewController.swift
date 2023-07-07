3c6e9419143246fcc2e4ef3504ab9eed6fa6a795
okue_hidetaka <okue@as-systems.jp>
2023-07-08 02:19:19 +0900

    firstCommit

---
+++ PokemonAlamofirePractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonAlamofirePractice
+//
+//  Created by 奥江英隆 on 2023/07/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
